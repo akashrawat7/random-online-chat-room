5e86b1371bbf615409ac0373c2aa21dbfb06ca6f
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2021-07-26 16:16:03 +0700

    Anti Sql injection attack
    No check data input lead to sql injection vulnerability.

--- chat-room/files/mes_if.php
+++ chat-room/files/mes_if.php
@@ -3,14 +3,13 @@
 include('connect.php');
 
 if(isset($_POST['msg'])){
-	$send_msg = $_POST['message'];
-	$name = $_SESSION['username'];
+	$send_msg = mysql_real_escape_string($_POST['message']);
+	$name = mysql_real_escape_string($_SESSION['username']);
 
 	$sql = "insert into chat(msg, name) values ('$send_msg','$name')";
 	$query = mysqli_query($con, $sql);
 
 	header("location:chat.php");
-
 }
 
 ?>
